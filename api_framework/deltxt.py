c8390a14872489d664d4ae168f292770c26eb15b
shangguanyi <249185549@qq.com>
2023-06-29 11:15:32 +0800

    update

--- api_framework/deltxt.py
+++ api_framework/deltxt.py
@@ -1,6 +1,6 @@
-#coding=gbk
+#encoding=utf-8
 import os
-# 清空文件内容
+# 娓呯┖鏂囦欢鍐呭
 def delwenjian():
     with open(
     os.getcwd() + '\\log\\log1.txt', 'w') as f1, open(os.getcwd() + '\\log\\log2.txt', 'w') as f2, open(os.getcwd() + '\\log\\log3.txt', 'w') as f3,open(os.getcwd() + '\\log\\log4.txt', 'w') as f4:
