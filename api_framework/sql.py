47699eca587cf72ad14d865fbda8dbcd9e26adaf
shangguanyi123 <88183379+shangguanyi123@users.noreply.github.com>
2024-02-19 14:06:15 +0800

    Update sql.py

--- api_framework/sql.py
+++ api_framework/sql.py
@@ -1,14 +1,15 @@
+#encoding=utf-8
 import pymysql
 
 class MySQLHelper:
     def __init__(self):
         #连接mysql
         self.connection = pymysql.connect(
-            host= '221.226.240.154',
-            port= 3307,
+            host= '45.33.49.209',
+            port= 3306,
             user= 'root',
-            password= '****',
-            database= 'qso_erp',
+            password= '**********',
+            database= 'a2b_new',
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor
         )
@@ -16,20 +17,23 @@ class MySQLHelper:
     def execute_query(self, query):
         result = None
         try:
+            self.connection.ping(reconnect=True)  # 检查连接是否断开，如果断开就进行重连
             with self.connection.cursor() as cursor: #获取游标对象
                 cursor.execute(query) #执行查询
                 result = cursor.fetchall() #获取当前结果
-        finally:
-            self.connection.commit() #提交事务，返回查询结果
-            return result
+        except pymysql.MySQLError as e:
+            print(f"Error executing query: {e}")
+        return result
     #执行更新语句（包括修改和删除操作）
     def execute_update(self, query):
         try:
+            self.connection.ping(reconnect=True)  # 检查连接是否断开，如果断开就进行重连
             with self.connection.cursor() as cursor: #获取游标对象
                 cursor.execute(query) #执行更新
+        except pymysql.MySQLError as e:
+            print(f"Error executing update: {e}")
         finally:
             self.connection.commit() #提交事务
     #关闭数据库连接
     def close_connection(self):
         self.connection.close() #关闭连接
-
